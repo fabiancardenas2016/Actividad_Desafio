c1b23e20e3d4d94206d8bf0fc240098193d027f9
fabiancardenas2016 <fabiancardenas2016@gmail.com>
2026-06-09 17:00:13 -0500

    Actividad desafio prueba piloto
    se agregan otros archivos

---
+++ Converter.java
@@ -0,0 +1,39 @@
+Unit_Converter
+
+Currency, Temperature, Surface, Length, Mass, Volume, and Time Converter
+
+This is a Java code that allows you to convert various units of measurement, including currency, temperature, surface, length, mass, volume, and time.
+
+
+Usage
+
+To use this code, simply run the Converter.java file. You will be prompted to select the type of conversion you would like to perform.
+Once you have selected the type of conversion, you will be prompted to enter the value you would like to convert. The program will then display the converted value.
+
+
+Supported Conversions
+
+This code supports the following conversions:
+
+    Currency: Converts between various currencies, including USD, EUR, JPY, GBP, and KRW.
+    Temperature: Converts between Celsius, Fahrenheit, and Kelvin.
+    Surface: Converts between square meters, square kilometers, square feet, and acres.
+    Length: Converts between meters, kilometers, feet, and miles.
+    Mass: Converts between grams, kilograms, pounds, and ounces.
+    Volume: Converts between liters, milliliters, cubic meters, cubic feet, and gallons.
+    Time: Converts between seconds, minutes, hours, days, weeks, and years.
+
+
+Customization
+
+This code can be easily customized to add additional conversions or change the existing conversions. Simply edit the Converter.java file to add or modify the conversion logic.
+
+
+Credits
+
+This code was written by SirLemaik. If you have any questions or issues with the code, please contact LonelyMurder#7595 on Discord.
+
+This is a Currency Converter project requested by Oracle in their schoolarship called Oracle Next Education held by Alura Latam, this project was made with JAVA and Intellij as IDE.
+
+
+![Captura de Pantalla 2023-02-23 a la(s) 21 22 11(1)](https://user-images.githubusercontent.com/115129617/221080283-10b91d68-014f-41b9-acad-6e35b4881fec.png)
